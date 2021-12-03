0fd526842076b03dc4c86b630d437a9ce86fab6e
fnmabreu@gmail.com <fnmabreu@gmail.com>
2021-12-03 20:03:30 +0000

    Failover Inventory Service

---
+++ src/main/java/guru/sfg/brewery/inventoryfailover/model/BeerInventoryDto.java
@@ -0,0 +1,25 @@
+package guru.sfg.brewery.inventoryfailover.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+/**
+ * Created by fabreu on 03/12/2021
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerInventoryDto {
+
+    private UUID id;
+    private OffsetDateTime createdDate;
+    private OffsetDateTime lastModifiedDate;
+    private UUID beerId;
+    private Integer quantityOnHand;
+}
