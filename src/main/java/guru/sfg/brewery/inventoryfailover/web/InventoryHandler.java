a79cdc4c9ad390574f7aa6c026345a8ec51aa7e8
fnmabreu@gmail.com <fnmabreu@gmail.com>
2021-12-04 11:54:53 +0000

    Add Hystrix Circuit Breaker for Inventory Service

--- src/main/java/guru/sfg/brewery/inventoryfailover/web/InventoryHandler.java
+++ src/main/java/guru/sfg/brewery/inventoryfailover/web/InventoryHandler.java
@@ -20,7 +20,7 @@ public class InventoryHandler {
 
     public Mono<ServerResponse> listInventory(ServerRequest request) {
         return ServerResponse.ok()
-                .contentType(MediaType.APPLICATION_NDJSON)
+                .contentType(MediaType.APPLICATION_STREAM_JSON)
                 .body(Mono.just(Arrays.asList(
                         BeerInventoryDto.builder()
                                 .id(UUID.randomUUID())
